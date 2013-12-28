c854cad06cb7c285a4aec81f27f1e7fbfde5f0fc
Matthew Chase Whittemore <matthew@socialtables.com>
2013-12-27 19:27:29 -0500

    basic usage is done

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var featureSubsetFinder = require("./lib/featureSubsetFinder");
 var makeFeatures = require("./lib/makeFeatures");
 var styleBuilder = require("./lib/styleBuilder");
 var scriptBuilder = require("./lib/scriptBuilder");
-
-var options = {};
+var routeBuilder = require("./lib/routeBuilder");
+var debug = require("debug")("featurlets");
 
 var FEATURLETS = module.exports = function(opts) {
 
@@ -13,7 +13,7 @@ var FEATURLETS = module.exports = function(opts) {
 		throw new Error("FOLDER MUST BE DEFINED");
 	}
 	else {
-		console.log("FEATURE FOLDER", opts.folder);
+		debug("FEATURE FOLDER", opts.folder);
 	}
 
 	opts.styles = opts.styles || /styles\.css$/;
@@ -31,10 +31,13 @@ var FEATURLETS = module.exports = function(opts) {
 
 	var features = makeFeatures(opts.folder);
 	var getFeatureSubset = featureSubsetFinder(opts.permissions, features);
+	var router = routeBuilder(features, getFeatureSubset);
 
 	var middleware = {};
 
+	var styles = {};
 	middleware.styleist = function * (next) {
+		debug(">>> STYLEIST");
 		if (opts.styles.test(this.path)) {
 			if (styles[this.permission] == undefined) {
 				var subset = getFeatureSubset(this.permission);
@@ -45,11 +48,14 @@ var FEATURLETS = module.exports = function(opts) {
 			this.body = styles[this.permission];
 		}
 		else {
-			yield next;
+			yield next || noop;
 		}
+		debug("<<< STYLEIST");
 	}
 
+	var scripts = {};
 	middleware.scriptor = function * (next) {
+		debug(">>> SCRIPTOR")
 		if (opts.scripts.test(this.path)) {
 			if (scripts[this.permission] == undefined) {
 				var subset = getFeatureSubset(this.permission);
@@ -60,17 +66,22 @@ var FEATURLETS = module.exports = function(opts) {
 			this.body = scripts[this.permission];
 		}
 		else {
-			yield next;
+			yield next || noop;
 		}
+		debug("<<< SCRIPTOR");
 	}
 
 	middleware.router = function * (next) {
-		var subset = getFeatureSubset(this.permission);
-
-		var foundRoute = router.call(this, subset);
-
-		if (!foundRoute) {
-			yield next;
-		}
+		debug(">>> ROUTER");
+		yield router.call(this, next || noop);
+		debug("<<< ROUTER");
 	}
+
+	middleware.all = function * (next) {
+		yield middleware.styleist.call(this, middleware.scriptor.call(this, middleware.router.call(this, next || noop)));
+	}
+
+	function noop() {};
+
+	return middleware;
 }
