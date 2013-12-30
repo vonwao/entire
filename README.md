11a37b3a61818535a00871c9eb3dabb06d07bc7a
Matthew Chase Whittemore <matthew@socialtables.com>
2013-12-30 15:50:01 -0500

    Move from featurlets to Entire
    Updated readme.md
    renamed files to match new naming
    renamed vars to match new naming
    commented to help clear up things

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Featurlets
+# Entire
 
 Build features for your websites like you build modules for node!
 
 ## Install
 
-`npm install https://github.com/mcwhittemore/featurlets/tarball/master`
+`npm install https://github.com/entirejs/entire/tarball/master`
 
 ## KOA Setup
 
@@ -13,9 +13,9 @@ var koa = require('koa');
 var app = koa();
 var path = require("path");
 
-var featurlets = require("featurlets");
+var entire = require("entire");
 
-app.use(featurlets({
+app.use(entire({
 	"folder": path.join(__dirname, "features")
 }).all);
 
@@ -25,7 +25,7 @@ app.listen(3000);
 
 ## Features
 
-The main brain of featurlets are the features. All features must be in a single feature folder which is passed to featurlets via the folder attribute of the featurlets opts param.
+The main brain of entire are the features. All features must be in a single feature folder which is passed to entire via the folder attribute of the entire opts param.
 
 A feature must be a folder and that folder must contain a feature.json file. Here is an example.
 
@@ -60,9 +60,9 @@ Comming Soon.
 
 ## Adding Permissions
 
-By default featurlets lets all users access all features, but this isn't always wanted.
+By default entire lets all users access all features, but this isn't always wanted.
 
-Permissions in featurlets is controlled by the this.permission param of the koa context. To set this add a peice of custom middleware that sets the permission of the user and than yields next.
+Permissions in entire is controlled by the this.permission param of the koa context. To set this add a peice of custom middleware that sets the permission of the user and than yields next.
 
 ```js
 app.use(function *(next){
@@ -77,10 +77,10 @@ app.use(function *(next){
 });
 ```
 
-Then, when setting up your featurlets middleware, add a permissions attribute to the opts arg. This permissions arg is a KVP where the key is a permission name and the value is an array of feature that permission can acceess.
+Then, when setting up your entire middleware, add a permissions attribute to the opts arg. This permissions arg is a KVP where the key is a permission name and the value is an array of feature that permission can acceess.
 
 ```js
-app.use(featurlets({
+app.use(entire({
 	"permissions": {
 		"boring": [
 			"index"
--- index.js
+++ index.js
@@ -1,94 +1,9 @@
-var fs = require("fs");
-var path = require("path");
-var featureSubsetFinder = require("./lib/featureSubsetFinder");
-var makeFeatures = require("./lib/makeFeatures");
-var styleBuilder = require("./lib/styleBuilder");
-var scriptBuilder = require("./lib/scriptBuilder");
-var routeBuilder = require("./lib/routeBuilder");
-var debug = require("debug")("featurlets");
-var reqMore = require("require-more")();
+var ENTIRE = require("./lib/entire");
 
-var FEATURLETS = module.exports = function(opts) {
-
-	if (!opts.folder) {
-		throw new Error("FOLDER MUST BE DEFINED");
-	}
-	else {
-		debug("FEATURE FOLDER", opts.folder);
+module.exports = function(opts){
+	if(opts){
+		ENTIRE.setup(opts);
 	}
 
-	reqMore(opts.folder);
-
-	opts.styles = opts.styles || /styles\.css$/;
-	opts.scripts = opts.scripts || /scripts\.js$/;
-
-	if (opts.styles instanceof RegExp !== true) {
-		throw new Error("opts.styles must be a RegExp");
-	}
-
-	if (opts.scripts instanceof RegExp !== true) {
-		throw new Error("opts.styles must be a RegExp");
-	}
-
-	opts.permissions = opts.permissions || true;
-
-	var features = makeFeatures(opts.folder);
-	var getFeatureSubset = featureSubsetFinder(opts.permissions, features);
-	var router = routeBuilder(features, getFeatureSubset);
-
-	var middleware = {};
-
-	var styles = {};
-	middleware.styleist = function * (next) {
-		debug(">>> STYLEIST");
-		if (opts.styles.test(this.path)) {
-			if (styles[this.permission] == undefined) {
-				debug(">>> BUILDING STYLE: " + this.permission);
-				var subset = getFeatureSubset(this.permission);
-				styles[this.permission] = yield styleBuilder(subset);
-				debug("<<< BUILDING STYLE: " + this.permission);
-			}
-
-			this.type = "text/css";
-			this.body = styles[this.permission];
-		}
-		else {
-			yield next || noop;
-		}
-		debug("<<< STYLEIST");
-	}
-
-	var scripts = {};
-	middleware.scriptor = function * (next) {
-		debug(">>> SCRIPTOR");
-		if (opts.scripts.test(this.path)) {
-			if (scripts[this.permission] == undefined) {
-				debug(">>> BUILDING SCRIPT: " + this.permission);
-				var subset = getFeatureSubset(this.permission);
-				scripts[this.permission] = yield scriptBuilder(subset);
-				debug("<<< BUILDING SCRIPT: " + this.permission);
-			}
-
-			this.type = "application/javascript";
-			this.body = scripts[this.permission];
-		}
-		else {
-			yield next || noop;
-		}
-		debug("<<< SCRIPTOR");
-	}
-
-	middleware.router = function * (next) {
-		debug(">>> ROUTER");
-		yield router.call(this, next || noop);
-		debug("<<< ROUTER");
-	}
-
-	middleware.all = function * (next) {
-		yield middleware.styleist.call(this, middleware.scriptor.call(this, middleware.router.call(this, next || noop)));
-	}
-
-	function noop() {};
-
-	return middleware;
+	return ENTIRE;
 }
